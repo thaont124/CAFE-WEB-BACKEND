d6394fb8a0e066541a326a93d658e35ac900743f
thao <thaont124@outlook.com.vn>
2025-07-30 08:26:05 +0700

    first commit

---
+++ product-service/src/test/java/lnt/be/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package lnt.be.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
